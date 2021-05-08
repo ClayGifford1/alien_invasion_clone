e35dc6bdd6d7b9ee28e2b762543de0886fc0040c
Clay <claygifford1@gmail.com>
2021-05-07 19:12:37 -0500

    fixed image paths and bullet color

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -12,7 +12,7 @@ class Ship(Sprite):
         self.ai_settings = ai_settings
 
         # Load the ship image and get its rect
-        self.image = pygame.image.load('ship.bmp')
+        self.image = pygame.image.load('alien_invasion/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
